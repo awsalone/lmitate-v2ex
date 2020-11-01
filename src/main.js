e50bf6e7b86c9535d787cf93d0f3c7c0897492ea
awsalone <1415591022@qq.com>
2020-11-01 23:33:33 +0800

    话题页及评论

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { Button, Select } from 'element-ui'
+import './plugins/element.js'
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
